602eed0a379aeb65505189e8af8d4fc4dfb94a42
tejude2000 <tejude2000@yahoo.com>
2022-08-18 14:52:12 -0500

    new file

--- fleetcart-website/rds.tf
+++ fleetcart-website/rds.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "database_subnet_group" {
 }
 
 # get the latest db snapshot
-data "aws_db_snapshot" "latest_db_snapshot" {
+data "aws_db_snapshot" "new_db_snapshot" {
   db_snapshot_identifier = var.database_snapshot_identifier
   most_recent            = true
   snapshot_type          = "manual"
@@ -22,7 +22,7 @@ resource "aws_db_instance" "database_instance" {
   skip_final_snapshot     = true
   availability_zone       = "us-east-1b"
   identifier              = var.database_instance_identifier
-  snapshot_identifier     = data.aws_db_snapshot.latest_db_snapshot.id
+  snapshot_identifier     = data.aws_db_snapshot.new_db_snapshot.id
   db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
   multi_az                = var.multi_az_deployment
   vpc_security_group_ids  = [aws_security_group.database_security_group.id]

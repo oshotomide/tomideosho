fe583a6490b626c7ca45805d3a1c1e9efd498107
tejude2000 <tejude2000@yahoo.com>
2022-08-17 23:13:00 -0500

    codebuild file

---
+++ cicd/apply-terraform.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# fail on any error
+set -eu
+
+# go back to the previous directory
+cd .. 
+
+cd fleetcart-website
+
+# initialize terraform
+terraform init
+
+# apply terraform
+terraform apply -auto-approve
+
+# # destroy terraform
+# terraform destroy -auto-approve
